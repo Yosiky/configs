83eed56c46746820c148753b04b025f0f847322b
eestelle <yosiky@list.ru>
2023-03-27 20:18:47 +0000

    update: add script for fast configured system

---
+++ setting.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp git-conf ~/.git_conf
+cp vimrc ~/.vimrc
+cp zshrc ~/.zshrc
